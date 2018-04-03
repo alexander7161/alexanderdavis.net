1efef4a1577d8e67e8c93246056867baa5435a05
Davis, Alexander (k1761218) <alexander.davis@kcl.ac.uk>
2018-04-03 22:29:15 +0100

    Added github time update for all projects

--- projectsAPIscript.js
+++ projectsAPIscript.js
@@ -1,18 +1,17 @@
 window.onload = function() {
-//var url = httpGet("https://api.github.com/zen");
-//console.log(url);
+getTimeSince('alexander7161/MyIndividualHealthCare', 'MIHCTime');
+getTimeSince('Futuramistic/Bot', 'AlzheimerAssistant');
+getTimeSince('alexander7161/Habitat-Simulator', 'Habitat');
+getTimeSince('alexander7161/stratford', 'stratford');
 
-getTimeSinceMIHC();
 }
-
-function getTimeSinceMIHC()
+function getTimeSince(repo, elementID)
 {
-  var time;
   // Create a request variable and assign a new XMLHttpRequest object to it.
   var request = new XMLHttpRequest();
 
   // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', 'http://api.github.com/repos/alexander7161/MyIndividualHealthCare/commits', true);
+  request.open('GET', 'http://api.github.com/repos/'+repo+'/commits', true);
 
   request.onload = function () {
     // Begin accessing JSON data here
@@ -24,14 +23,13 @@ function getTimeSinceMIHC()
   });
   var date1 = new Date(commits[0].commit.author.date)
   var date2 = new Date();
-    console.log(date1);
-    console.log(date2);
+//    console.log(date1);
+//    console.log(date2);
     timeDiff = Math.abs(date2.getTime() - date1.getTime());
     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-  var element = document.getElementById("MIHCTime");
+  var element = document.getElementById(elementID);
   element.innerHTML = "Last updated " + diffDays + " days ago";
 }
   // Send request
   request.send();
-  return time;
 }
