1130b7d34d3bfd29f233f27f355658fc62e601ef
Davis, Alexander (k1761218) <alexander.davis@kcl.ac.uk>
2018-04-03 22:10:04 +0100

    github api added
    Projects last updated time auto update

--- projectsAPIscript.js
+++ projectsAPIscript.js
@@ -2,11 +2,36 @@ window.onload = function() {
 //var url = httpGet("https://api.github.com/zen");
 //console.log(url);
 
+getTimeSinceMIHC();
+}
 
-function httpGet(theUrl)
+function getTimeSinceMIHC()
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, true);
-    xmlHttp.send();
-    return xmlHttp.responseText;
+  var time;
+  // Create a request variable and assign a new XMLHttpRequest object to it.
+  var request = new XMLHttpRequest();
+
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', 'http://api.github.com/repos/alexander7161/MyIndividualHealthCare/commits', true);
+
+  request.onload = function () {
+    // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  var commits = [];
+  data.forEach(commit => {
+    // Log each movie's title
+  commits.push(commit);
+  });
+  var date1 = new Date(commits[0].commit.author.date)
+  var date2 = new Date();
+    console.log(date1);
+    console.log(date2);
+    timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  var element = document.getElementById("MIHCTime");
+  element.innerHTML = "Last updated " + diffDays + " days ago";
+}
+  // Send request
+  request.send();
+  return time;
 }
